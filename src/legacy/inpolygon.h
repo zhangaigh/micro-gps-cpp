0397c4bada35e57eba9319b2e858631979d2345e
lg-zhang <zhanglg921@gmail.com>
2018-09-17 17:02:44 -0400

    remove legacy

--- src/legacy/inpolygon.h
+++
@@ -1,9 +0,0 @@
-#ifndef _INPOLYGON_H_
-#define _INPOLYGON_H_
-
-#include <stdbool.h>
-
-void inpolygon(int nP, double* px, double* py, double* cx, double* cy, 
-              bool* points_in_on, bool* points_in, bool* points_on);
-
-#endif
