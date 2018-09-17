7d4b156b1db750b9a761f2a7293f5b70bef1c60d
lg-zhang <zhanglg921@gmail.com>
2018-09-17 14:14:17 -0400

    remove scripts

--- scripts/remove_png_gamma.m
+++
@@ -1,8 +0,0 @@
-clear
-% im_path = '/Users/lgzhang/Dropbox/Research/micro_gps/micro-gps-siggraph-asia/images/footprint/footprints_cropped.png';
-% im_path = '/Users/lgzhang/Dropbox/Research/micro_gps/micro-gps-siggraph-asia/images/footprint/footprints.png';
-im_path = '/Users/lgzhang/Dropbox/Research/micro_gps/micro-gps-siggraph-asia/images/footprint/warped_image_sample.png';
-
-
-im = imread(im_path);
-imwrite(im, im_path);
